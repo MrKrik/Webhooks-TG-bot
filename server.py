0eacda1bfa4757ffb3949aa2cd897df0d5004e3f
MRK <MRQ>
2024-10-19 02:46:53 +0300

    quark work

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
-from flask import Flask, request
+from quart import Quart, request
 from main import webhook_test
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 @app.route("/")
-def hello():
+async def hello():
     return "Hello World!"
 
-@app.route('/webhooks', methods = ['POST'])
+@app.route('/webhooks', methods=['POST'])
 async def submit():
     # Обработка запроса
-    name = request.form['key']
+    name = (await request.form)['key']
     # Передача в бота
     await webhook_test(name)
     print(name)
     return f'Hello, {name}'
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
